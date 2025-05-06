ac2732ceda6f6c6f0db20ab3ebd04823b2f0a0cc
Abelardo Rivera <158202356+Lalo277@users.noreply.github.com>
2025-05-05 18:36:54 -0600

    Update anim.js

--- anim.js
+++ anim.js
@@ -50,15 +50,15 @@ var lyricsData = [
 
 // Animar las letras
 function updateLyrics() {
-  var delayOffset = 0.8; // Puedes ir afinando este número
+  var delayOffset = 0.;8 // Puedes ir afinando este número
 var time = audio.currentTime - delayOffset;
   var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 3
+    (line) => time >= line.time && time < line.time + 5
   );
 
   if (currentLine) {
     // Calcula la opacidad basada en el tiempo en la línea actual
-    var fadeInDuration = 0.8; // Duración del efecto de aparición en segundos
+    var fadeInDuration = 0.5; // Duración del efecto de aparición en segundos
     var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
 
     // Aplica el efecto de aparición
